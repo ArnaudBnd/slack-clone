8294e5ad5b7ac2bd428688fa7bfb2281131809e2
arnaud <arnaudbenede@hotmail.fr>
2020-08-11 18:54:30 +0200

    add sidebar options

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Sidebar.css';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import SidebarOption from './SidebarOption.js';
 
 function Sidebar() {
   return (
     <div className="sidebar">
         <div className="sidebar__header">
             <div className="sidebar__info">
-                <h2>Clever Programmer</h2>
+                <h2>MyDigitalSchool</h2>
                 <h3>
                     <FiberManualRecordIcon />
                     Arnaud BENEDE
@@ -16,6 +18,20 @@ function Sidebar() {
             </div>
             <CreateIcon />
         </div>
+        <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+        <SidebarOption title="Youtube" />
+        <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+        <SidebarOption title="Youtube" />
+        <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+        <SidebarOption title="Youtube" />
+        <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+        <SidebarOption title="Youtube" />
+        <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+        <SidebarOption title="Youtube" />
+        <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+        <SidebarOption title="Youtube" />
+        <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+        <SidebarOption title="Youtube" />
     </div>
   );
 }
